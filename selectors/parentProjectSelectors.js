5c33d291d1ef17078c69d7d68b94bb4c76db2030
sniwe <rhys.ewins@gmail.com>
2024-09-23 02:15:26 -0700

    Update parentProjectSelectors.js

---
+++ selectors/parentProjectSelectors.js
@@ -0,0 +1,77 @@
+const parentProjectSelectors = {
+  LadyGrayy: {
+      targetUrl: 'https://trendbase.wixstudio.io/tb-redo/pcd',
+      dropdown: 'comp-lz88ome0',
+      optionText: 'Lady Grayy', 
+      searchInput: 'input_comp-lz88omfn4',
+      pageAssets: ['img-comp-lz88omay8','img-comp-lz88omb03','img-comp-lz9hnh3m1','img-comp-lz9mvetw2','img-comp-lz9mveu2','img-comp-lz9lgz42','img-comp-lz88omfy4','img-comp-lz88omdl4','img-comp-lz88omdt1','img-comp-lz88omd95','img-comp-lz88omet8'],
+      sections: ['comp-lz88omao','comp-lz9ft1fb','comp-lz9mvet4','comp-lz9lgz39','comp-lz9oqkk5','comp-lz88omfx5','comp-lz88omd011','comp-lz88ome9','comp-lz88ombj7','comp-lz88omc86'],
+      googleDriveConfig: {
+          "type": "service_account",
+          "project_id": "descimgup",
+          "private_key_id": "c91f926f98e74967793a16ee7b7c723b83ac6e81",
+          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCLzB5PP89VBV+4\npiuiI/MsIuBD3xvbsi236KNl6iXkVMZ3/95aGHal8kscQRPRn1s6FeU+9Q3Gy3Iu\n1s1U+eS3lc3ik2KoheY9BCc0xvH7eWT6oQqnk5I21hMcbjEOkWeSO9ew6n5v7vId\n5aN6TeiDPl/Nh2Jt/wg5sYHZwbeiLNqfKLJBqpHlbmETKQFtAzG8WZUs6ycdLrY+\ndtyXHXI1TJxEeAvmyuco32b+6gyPLQEQQi1dg9i7o+HVubiIcN2tbK4eq7ZlAF4a\nmq21nGxz9W9kXo/TYcADfHUH7dsxaXl693o0Xdhnsu/SEe7v3mvqSuglCbeXi8yh\nb0roZ0t/AgMBAAECggEAB1nGRjh+uGO98sGaDb6CLNHwpOD4YAbwbHkpDlTPhUAH\n54tmr6oVowgoduXdGuqcK2O3WCYegVN1P40/4rX5FsYju0QwrdfMkWzEevvzAVwS\nuTL6knEqAQQkoRwSLEftAD1xS/Yoi84uoO/QNsAd97ix5Oa3gD0mu5Hf2AldlINf\nXfBHzY9hGpa8ggxKh00MIvbGNDA1TmBp1mKuhYk2Shx0/O060NQuKX2f/YGCtYlP\n3zELYZB+VkpZ/Xd9tR6bmH1+pRNWIna+ckkF8+rkWgmw1+WZtWswnUAX6eCqCxQc\nf6DPbiS37kPw+TWeHz3lYCV9ZGXlrvgHC+vxBxQoYQKBgQC/7PTCLPYhPCFScE3m\nYzaWXP8vDalJ7JWiKSaohtRMuCJfkPZ8FYXKry4ngA4tOv4fBlBR0/JNe0FadbzX\nspgIu8WgVJBgqpw8522O35nbObWXs747qtVbCnBM7KKxhihEyskXeK12Xe1zfwHI\nuJFLKzIsFCQoPC8PubpEvXRtWQKBgQC6d/yJkuiKqjRPzbxOoAsNLrNPd3tmusvg\nUZjwccXQ/4+iX9cpcVz1bHwYe+ylTj0KtjosK4cmmx1rTqSJIQELF+eZ2zHULs+h\n8M0fEDCbQwMoP/idCNWkFfe3CnYbkrpUDrpqiXVkV6LGYSJTUeU52l7l0yQa9Lde\nj9L3YfuslwKBgQC3AWBFgw/Skv6HGDT/mNzlNZnSKSmiJqhrrueAzMfEu9Ny4sxV\nd0RTBwVwfseF03ma5a9Sx1GFFiEz+45s8ovL0BhakCf3NNezr00lbgrMIlQIakjb\npkK5A9QfVYiw0+az6dUjv1tPwbJ4bcOqvGs2jOLTawUrJHAPg56cizZoSQKBgQCa\n4jcdfq0xIJc7AkS7eiwz0uqVw0T424ek16IVdis9okkIMlthzQvNv0KEg8rYs9f4\nIczIC4/C/Wy+SSBZ1tQrBrwVDACjog5NENsQRj5ZeJwSvrAen8v081pC66mW1ZV3\nZJRrEao0oqZN6Ti9xHSGTWAtvedrjIETRpuRaA3IGQKBgQC36c1By95f+LzIfsky\nmYxR3FEH3ZhlI0FHEWoMWA8VsvMUk6zUTswoX0BjkjmNT5S1ak6ixn7IeZxcVU49\nANCKi5OxScNzrb+hv7PEJ7cPHAAaWIUmEYJJTdyFdN+lFlD+ck7Y7wrRBTcXVQRy\nhbjCii54a4GglVpyWWkimCGdIg==\n-----END PRIVATE KEY-----\n",
+          "client_email": "storage@descimgup.iam.gserviceaccount.com",
+          "client_id": "109891450527331825165",
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/storage%40descimgup.iam.gserviceaccount.com",
+          "universe_domain": "googleapis.com",
+          "drive_folder_id": "1u_RWQIMdG8yxI8gbPw5DxO0xTWGqlCMV"
+        },
+      watermarkUrl: 'https://static.wixstatic.com/media/7c16b9_ca023ca3f322498e9471954ad63b08d1~mv2.png'
+  },
+  Santeco: {
+      targetUrl: 'https://trendbase.wixstudio.io/tb-redo/pcd',
+      dropdown: 'comp-lz88ome0',
+      optionText: 'Santeco', 
+      searchInput: 'input_comp-lz88omfn4',
+      pageAssets: ["img-comp-lz88omjh16","img-comp-lz88om8l2","img-comp-lz88om9x2"],
+      sections: ["comp-lz88omi8","comp-lz88om8e","comp-lz88om9s4","comp-lz88omgv3"],
+      googleDriveConfig: {
+          "type": "service_account",
+          "project_id": "descimgup",
+          "private_key_id": "c91f926f98e74967793a16ee7b7c723b83ac6e81",
+          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCLzB5PP89VBV+4\npiuiI/MsIuBD3xvbsi236KNl6iXkVMZ3/95aGHal8kscQRPRn1s6FeU+9Q3Gy3Iu\n1s1U+eS3lc3ik2KoheY9BCc0xvH7eWT6oQqnk5I21hMcbjEOkWeSO9ew6n5v7vId\n5aN6TeiDPl/Nh2Jt/wg5sYHZwbeiLNqfKLJBqpHlbmETKQFtAzG8WZUs6ycdLrY+\ndtyXHXI1TJxEeAvmyuco32b+6gyPLQEQQi1dg9i7o+HVubiIcN2tbK4eq7ZlAF4a\nmq21nGxz9W9kXo/TYcADfHUH7dsxaXl693o0Xdhnsu/SEe7v3mvqSuglCbeXi8yh\nb0roZ0t/AgMBAAECggEAB1nGRjh+uGO98sGaDb6CLNHwpOD4YAbwbHkpDlTPhUAH\n54tmr6oVowgoduXdGuqcK2O3WCYegVN1P40/4rX5FsYju0QwrdfMkWzEevvzAVwS\nuTL6knEqAQQkoRwSLEftAD1xS/Yoi84uoO/QNsAd97ix5Oa3gD0mu5Hf2AldlINf\nXfBHzY9hGpa8ggxKh00MIvbGNDA1TmBp1mKuhYk2Shx0/O060NQuKX2f/YGCtYlP\n3zELYZB+VkpZ/Xd9tR6bmH1+pRNWIna+ckkF8+rkWgmw1+WZtWswnUAX6eCqCxQc\nf6DPbiS37kPw+TWeHz3lYCV9ZGXlrvgHC+vxBxQoYQKBgQC/7PTCLPYhPCFScE3m\nYzaWXP8vDalJ7JWiKSaohtRMuCJfkPZ8FYXKry4ngA4tOv4fBlBR0/JNe0FadbzX\nspgIu8WgVJBgqpw8522O35nbObWXs747qtVbCnBM7KKxhihEyskXeK12Xe1zfwHI\nuJFLKzIsFCQoPC8PubpEvXRtWQKBgQC6d/yJkuiKqjRPzbxOoAsNLrNPd3tmusvg\nUZjwccXQ/4+iX9cpcVz1bHwYe+ylTj0KtjosK4cmmx1rTqSJIQELF+eZ2zHULs+h\n8M0fEDCbQwMoP/idCNWkFfe3CnYbkrpUDrpqiXVkV6LGYSJTUeU52l7l0yQa9Lde\nj9L3YfuslwKBgQC3AWBFgw/Skv6HGDT/mNzlNZnSKSmiJqhrrueAzMfEu9Ny4sxV\nd0RTBwVwfseF03ma5a9Sx1GFFiEz+45s8ovL0BhakCf3NNezr00lbgrMIlQIakjb\npkK5A9QfVYiw0+az6dUjv1tPwbJ4bcOqvGs2jOLTawUrJHAPg56cizZoSQKBgQCa\n4jcdfq0xIJc7AkS7eiwz0uqVw0T424ek16IVdis9okkIMlthzQvNv0KEg8rYs9f4\nIczIC4/C/Wy+SSBZ1tQrBrwVDACjog5NENsQRj5ZeJwSvrAen8v081pC66mW1ZV3\nZJRrEao0oqZN6Ti9xHSGTWAtvedrjIETRpuRaA3IGQKBgQC36c1By95f+LzIfsky\nmYxR3FEH3ZhlI0FHEWoMWA8VsvMUk6zUTswoX0BjkjmNT5S1ak6ixn7IeZxcVU49\nANCKi5OxScNzrb+hv7PEJ7cPHAAaWIUmEYJJTdyFdN+lFlD+ck7Y7wrRBTcXVQRy\nhbjCii54a4GglVpyWWkimCGdIg==\n-----END PRIVATE KEY-----\n",
+          "client_email": "storage@descimgup.iam.gserviceaccount.com",
+          "client_id": "109891450527331825165",
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/storage%40descimgup.iam.gserviceaccount.com",
+          "universe_domain": "googleapis.com",
+          "drive_folder_id": "1u_RWQIMdG8yxI8gbPw5DxO0xTWGqlCMV"
+        },
+      watermarkUrl: 'https://static.wixstatic.com/media/7c16b9_a59efc66950746abbaf46ed19b2e79f9~mv2.png'
+  },
+  Max: {
+      targetUrl: 'https://trendbase.wixstudio.io/tb-redo/pcd',
+      dropdown: 'comp-lz88ome0',
+      optionText: 'Max', 
+      searchInput: 'input_comp-lz88omfn4',
+      pageAssets: ["img-comp-m0w6826k","img-comp-m0qhs6lf5__0","img-comp-m0qhs6lf5__1","img-comp-m0qhs6lf5__2","img-comp-m0qhs6lf5__3","img-comp-m0qhs6lf5__4","img-comp-m0qhs6lf5__5","img-comp-m0qhs6lf5__6","img-comp-m0qhs6lf5__7",
+                  "img-comp-m0ywo15c","img-comp-m0w7b3zz5__0","img-comp-m0w7b3zz5__1","img-comp-m0w7b3zz5__2","img-comp-m0w7b3zz5__3","img-comp-m0w7b3zz5__4","img-comp-m0w7b3zz5__5",
+                  "img-comp-m0w7silu5__0","img-comp-m0w7silu5__1","img-comp-m0w7silu5__2","img-comp-m0w7silu5__3","img-comp-m0w7silu5__4","img-comp-m0w7silu5__5","img-comp-m0w7silu5__6","img-comp-m0w7silu5__7","img-comp-m0w7silu5__8",
+                  "img-comp-m0z4w44v__0","img-comp-m0z4w44v__1","img-comp-m0z4w44v__2","img-comp-m0z4w44v__3","img-comp-m0z4w44v__4","img-comp-m0z4w44v__5","img-comp-m0z4w44v__6","img-comp-m0z4w44v__7","img-comp-m0z4w44v__8",
+                  "img-comp-m0xiftrb1","img-comp-m0xkp8i72__0","img-comp-m0xkp8i72__1","img-comp-m0xkp8i72__2","img-comp-m0xkp8i72__3","img-comp-m0xlas5m__0","img-comp-m0xlas5m__1","img-comp-m0xlas5m__2"],
+      sections: ["comp-m11m2o6d","comp-m18t0biz","comp-m0w7silb__0","comp-m0w7silb__1","comp-m0w7silb__2","comp-m0w7silb__3",,"comp-m0w7silb__4","comp-m0w7silb__5","comp-m0w7silb__6","comp-m0w7silb__7","comp-m0w7silb__8","comp-m11lxar1"],
+      googleDriveConfig: {
+          "type": "service_account",
+          "project_id": "descimgup",
+          "private_key_id": "c91f926f98e74967793a16ee7b7c723b83ac6e81",
+          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCLzB5PP89VBV+4\npiuiI/MsIuBD3xvbsi236KNl6iXkVMZ3/95aGHal8kscQRPRn1s6FeU+9Q3Gy3Iu\n1s1U+eS3lc3ik2KoheY9BCc0xvH7eWT6oQqnk5I21hMcbjEOkWeSO9ew6n5v7vId\n5aN6TeiDPl/Nh2Jt/wg5sYHZwbeiLNqfKLJBqpHlbmETKQFtAzG8WZUs6ycdLrY+\ndtyXHXI1TJxEeAvmyuco32b+6gyPLQEQQi1dg9i7o+HVubiIcN2tbK4eq7ZlAF4a\nmq21nGxz9W9kXo/TYcADfHUH7dsxaXl693o0Xdhnsu/SEe7v3mvqSuglCbeXi8yh\nb0roZ0t/AgMBAAECggEAB1nGRjh+uGO98sGaDb6CLNHwpOD4YAbwbHkpDlTPhUAH\n54tmr6oVowgoduXdGuqcK2O3WCYegVN1P40/4rX5FsYju0QwrdfMkWzEevvzAVwS\nuTL6knEqAQQkoRwSLEftAD1xS/Yoi84uoO/QNsAd97ix5Oa3gD0mu5Hf2AldlINf\nXfBHzY9hGpa8ggxKh00MIvbGNDA1TmBp1mKuhYk2Shx0/O060NQuKX2f/YGCtYlP\n3zELYZB+VkpZ/Xd9tR6bmH1+pRNWIna+ckkF8+rkWgmw1+WZtWswnUAX6eCqCxQc\nf6DPbiS37kPw+TWeHz3lYCV9ZGXlrvgHC+vxBxQoYQKBgQC/7PTCLPYhPCFScE3m\nYzaWXP8vDalJ7JWiKSaohtRMuCJfkPZ8FYXKry4ngA4tOv4fBlBR0/JNe0FadbzX\nspgIu8WgVJBgqpw8522O35nbObWXs747qtVbCnBM7KKxhihEyskXeK12Xe1zfwHI\nuJFLKzIsFCQoPC8PubpEvXRtWQKBgQC6d/yJkuiKqjRPzbxOoAsNLrNPd3tmusvg\nUZjwccXQ/4+iX9cpcVz1bHwYe+ylTj0KtjosK4cmmx1rTqSJIQELF+eZ2zHULs+h\n8M0fEDCbQwMoP/idCNWkFfe3CnYbkrpUDrpqiXVkV6LGYSJTUeU52l7l0yQa9Lde\nj9L3YfuslwKBgQC3AWBFgw/Skv6HGDT/mNzlNZnSKSmiJqhrrueAzMfEu9Ny4sxV\nd0RTBwVwfseF03ma5a9Sx1GFFiEz+45s8ovL0BhakCf3NNezr00lbgrMIlQIakjb\npkK5A9QfVYiw0+az6dUjv1tPwbJ4bcOqvGs2jOLTawUrJHAPg56cizZoSQKBgQCa\n4jcdfq0xIJc7AkS7eiwz0uqVw0T424ek16IVdis9okkIMlthzQvNv0KEg8rYs9f4\nIczIC4/C/Wy+SSBZ1tQrBrwVDACjog5NENsQRj5ZeJwSvrAen8v081pC66mW1ZV3\nZJRrEao0oqZN6Ti9xHSGTWAtvedrjIETRpuRaA3IGQKBgQC36c1By95f+LzIfsky\nmYxR3FEH3ZhlI0FHEWoMWA8VsvMUk6zUTswoX0BjkjmNT5S1ak6ixn7IeZxcVU49\nANCKi5OxScNzrb+hv7PEJ7cPHAAaWIUmEYJJTdyFdN+lFlD+ck7Y7wrRBTcXVQRy\nhbjCii54a4GglVpyWWkimCGdIg==\n-----END PRIVATE KEY-----\n",
+          "client_email": "storage@descimgup.iam.gserviceaccount.com",
+          "client_id": "109891450527331825165",
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/storage%40descimgup.iam.gserviceaccount.com",
+          "universe_domain": "googleapis.com",
+          "drive_folder_id": "1u_RWQIMdG8yxI8gbPw5DxO0xTWGqlCMV"
+        },
+      watermarkUrl: ''
+  }
+};
+
+module.exports = parentProjectSelectors;
