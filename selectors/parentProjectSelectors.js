3cb26c7b1fe06cf8a46c84c58194fa0ecc591edf
sniwe <rhys.ewins@gmail.com>
2024-12-11 12:03:06 +0800

    Update parentProjectSelectors.js

--- selectors/parentProjectSelectors.js
+++ selectors/parentProjectSelectors.js
@@ -7,19 +7,18 @@ const parentProjectSelectors = {
       pageAssets: ["img-comp-m4j96g43","img-comp-lz88omfy4","img-comp-lz88omdl4","img-comp-lz88omdt1","img-comp-lz88omd95","img-comp-lz88omet8","img-comp-lz88omf33"],
       sections: ["comp-m4j96g39","comp-lz88omfx5","comp-lz88omd011","comp-lz88ome9"],
       googleDriveConfig: {
-          "type": "service_account",
-          "project_id": "descimgup",
-          "private_key_id": "c91f926f98e74967793a16ee7b7c723b83ac6e81",
-          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCLzB5PP89VBV+4\npiuiI/MsIuBD3xvbsi236KNl6iXkVMZ3/95aGHal8kscQRPRn1s6FeU+9Q3Gy3Iu\n1s1U+eS3lc3ik2KoheY9BCc0xvH7eWT6oQqnk5I21hMcbjEOkWeSO9ew6n5v7vId\n5aN6TeiDPl/Nh2Jt/wg5sYHZwbeiLNqfKLJBqpHlbmETKQFtAzG8WZUs6ycdLrY+\ndtyXHXI1TJxEeAvmyuco32b+6gyPLQEQQi1dg9i7o+HVubiIcN2tbK4eq7ZlAF4a\nmq21nGxz9W9kXo/TYcADfHUH7dsxaXl693o0Xdhnsu/SEe7v3mvqSuglCbeXi8yh\nb0roZ0t/AgMBAAECggEAB1nGRjh+uGO98sGaDb6CLNHwpOD4YAbwbHkpDlTPhUAH\n54tmr6oVowgoduXdGuqcK2O3WCYegVN1P40/4rX5FsYju0QwrdfMkWzEevvzAVwS\nuTL6knEqAQQkoRwSLEftAD1xS/Yoi84uoO/QNsAd97ix5Oa3gD0mu5Hf2AldlINf\nXfBHzY9hGpa8ggxKh00MIvbGNDA1TmBp1mKuhYk2Shx0/O060NQuKX2f/YGCtYlP\n3zELYZB+VkpZ/Xd9tR6bmH1+pRNWIna+ckkF8+rkWgmw1+WZtWswnUAX6eCqCxQc\nf6DPbiS37kPw+TWeHz3lYCV9ZGXlrvgHC+vxBxQoYQKBgQC/7PTCLPYhPCFScE3m\nYzaWXP8vDalJ7JWiKSaohtRMuCJfkPZ8FYXKry4ngA4tOv4fBlBR0/JNe0FadbzX\nspgIu8WgVJBgqpw8522O35nbObWXs747qtVbCnBM7KKxhihEyskXeK12Xe1zfwHI\nuJFLKzIsFCQoPC8PubpEvXRtWQKBgQC6d/yJkuiKqjRPzbxOoAsNLrNPd3tmusvg\nUZjwccXQ/4+iX9cpcVz1bHwYe+ylTj0KtjosK4cmmx1rTqSJIQELF+eZ2zHULs+h\n8M0fEDCbQwMoP/idCNWkFfe3CnYbkrpUDrpqiXVkV6LGYSJTUeU52l7l0yQa9Lde\nj9L3YfuslwKBgQC3AWBFgw/Skv6HGDT/mNzlNZnSKSmiJqhrrueAzMfEu9Ny4sxV\nd0RTBwVwfseF03ma5a9Sx1GFFiEz+45s8ovL0BhakCf3NNezr00lbgrMIlQIakjb\npkK5A9QfVYiw0+az6dUjv1tPwbJ4bcOqvGs2jOLTawUrJHAPg56cizZoSQKBgQCa\n4jcdfq0xIJc7AkS7eiwz0uqVw0T424ek16IVdis9okkIMlthzQvNv0KEg8rYs9f4\nIczIC4/C/Wy+SSBZ1tQrBrwVDACjog5NENsQRj5ZeJwSvrAen8v081pC66mW1ZV3\nZJRrEao0oqZN6Ti9xHSGTWAtvedrjIETRpuRaA3IGQKBgQC36c1By95f+LzIfsky\nmYxR3FEH3ZhlI0FHEWoMWA8VsvMUk6zUTswoX0BjkjmNT5S1ak6ixn7IeZxcVU49\nANCKi5OxScNzrb+hv7PEJ7cPHAAaWIUmEYJJTdyFdN+lFlD+ck7Y7wrRBTcXVQRy\nhbjCii54a4GglVpyWWkimCGdIg==\n-----END PRIVATE KEY-----\n",
-          "client_email": "storage@descimgup.iam.gserviceaccount.com",
-          "client_id": "109891450527331825165",
-          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-          "token_uri": "https://oauth2.googleapis.com/token",
-          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/storage%40descimgup.iam.gserviceaccount.com",
-          "universe_domain": "googleapis.com",
-          "drive_folder_id": "1u_RWQIMdG8yxI8gbPw5DxO0xTWGqlCMV"
-        },
+        "type": "service_account",
+        "project_id": "driveup-410108",
+        "private_key_id": "68056b4e45406873c910e29b96f63cccb050d100",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCuc7q0tjKH+gFR\nAYEFIuJ56aMANY6uhU10HI7RY3nHZ4HbgXHx89jtq57DEQgxyRzsdm9RbnlSdueI\nLmw+w5nxzB0MNsbNN6BdC6hWKD8bOqiTHsm6ToA/5UMgQkn9V9eryESySWrM9XVb\n5kTI262oPR7l5JZkUVxwrRHvim+edXdsHe0pISeLChnyvx5xZBLAJmlPwl+fiKFx\ngzFOFXgW+SXKiNHqgoVYBm4lj208CeeAIEyv1YC1/7HwtO+KSD25ZpAAHqy+r0D7\n2wP+zxtbG9XsvkEmvAWwWzU4Er0Oo1eug8vlYkj5V2TNuVGfLRuitzovFAXGj1M4\ngrpQgpXxAgMBAAECggEAH9fuoP1wAFudgWaboS8itXu0/f0IuCkLgBIVbLmyeThW\n3xVEEhT/iDSvRG6XtxS9k5kzzSK0AR5EuZo1tog4Ucd/FuNpyvxoKIvP9Lgges0s\nh7xTOM+K//7Oj7Uh8d1muAvUpFGb7eExRK7l598XlhHgEbk1zYZ/b+7biwTTRpJf\nsOZQXxJWlWKL2wy9uSdrAkSK5MiN63WzHQpFJqKOwNREdtiuCN6EiAo2YRV/7JMf\nA7cvk2Zb9kM/IK5zIzLM7mWSTB7VEjwrVyMJna32sB7Lud7TxoK+IxwhB3a9tc4j\nIiKKLMeDak/vjZQT8Sb1tk1Gy/W+fec6/P4GJZeuFQKBgQDDbN2u9oSBQG4ViLoQ\nModrY4BV8lb+MslKnmYdtocVtq/PRX+Eemxei6AEE2O/NQQH8TIVB5pNoRsHxg3r\nUG+yBuq7PodSYadcLHCWAUsYRgqc8tbMDpAoVlAEqtjyoUGOpT71kBebPftawhkE\neTjaRu544Wh4OYrkNZVzGZ8RZQKBgQDkhqAwNyTow9jV57Fw4RnoNxtq89hHlDS9\n9gZBRDiW9S+B7TktIkPijalT6Rir2THDJJdA1JmFni3OXEmZh2fBZqoPpO3G9H5C\ndbBsQJSwzcN8YcLqCBKvRp4ji0GsPe5nidW/8pIRxi4n2t/tgf8hGR/KiaW+cjb3\nVXcqITAPnQKBgHZLlRmPAgCXdsu0OyUI4S5hSdM84en35h80KQ49aiH7twqZwCn6\nTVUh4df1wMCCoBEKsM1ID+CD8C/RdKWHyEUUqMx1ekak9ED4aaJz3/gulcgRu3JE\nMoEoVkGtBaxBbsShRK4y7gc8CsYSjkBF/OMsYrhrgeictHWV9gR5cu99AoGAQ7lZ\n87AD8i9ILWwye79aDCV4rKODLwNO9hTZGbJ8CtCyuZBx51xGPfe5fLKRqyv8wVYw\ntPfMX3rXHFbhQOyN5RTlxSkdhl6ipFN4azxgVF1GV7ApMW4fC26kIuh8F+1qn6iL\nfg0HGNlgiFMnFZ322/o0H7PXOK9kdrwPSp73PUkCgYAfIv8xtw7b9p7n4BR1/3m0\nepFix4g3RoV3FarA07B/NtyVqjZQg0Ee56UNlJtWITOcWmVoYMAnixqdTlR57PgO\n/cz/61sRKv4mHt8gVofrD+sOB8IHWtIY3JoyKcO2JbtFlzpcun4DdaAi43eHa700\nDOOL2Xzzn/CNmBrU8fc0Yw==\n-----END PRIVATE KEY-----\n",
+        "client_email": "test1-417@driveup-410108.iam.gserviceaccount.com",
+        "client_id": "110220907911702729576",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/test1-417%40driveup-410108.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com"
+      },
       watermarkUrl: 'https://static.wixstatic.com/media/7c16b9_ca023ca3f322498e9471954ad63b08d1~mv2.png'
   },
   Santeco: {
